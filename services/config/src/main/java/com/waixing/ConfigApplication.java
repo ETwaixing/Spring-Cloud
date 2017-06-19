03814ed8c5e8c9763b755e146169c238a2b84b44
waixing <waixing@yonglang.co>
2017-06-19 19:06:16 +0800

    提交

--- services/config/src/main/java/com/waixing/ConfigApplication.java
+++ services/config/src/main/java/com/waixing/ConfigApplication.java
@@ -1,9 +1,10 @@
 package com.waixing;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
 
